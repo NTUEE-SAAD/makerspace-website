347839227382855c5391954466af96e840bf0621
zhuang-jia-xu <76544194+zhuang-jia-xu@users.noreply.github.com>
2022-01-12 02:29:27 +0800

    define custom text components

---
+++ frontend/src/components/Text.js
@@ -0,0 +1,9 @@
+// import styled from "styled-components";
+import { Typography } from "antd";
+
+const White = ({ children }) => (
+  <Typography style={{ color: "white" }}>{children}</Typography>
+);
+export const Text = {
+  White,
+};
