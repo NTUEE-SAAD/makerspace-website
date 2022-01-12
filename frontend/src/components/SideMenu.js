712a12c0bdd8550e44d3a4bc4e4571ad281deb7a
BingCheng <b09901044@ntu.edu.tw>
2022-01-13 00:18:45 +0800

    SideMenu

--- frontend/src/components/SideMenu.js
+++ frontend/src/components/SideMenu.js
@@ -1,5 +1,5 @@
-import { Menu, Dropdown,Button } from 'antd';
-import { DownOutlined } from '@ant-design/icons';
+import { Menu, Dropdown, Button } from "antd";
+import { DownOutlined } from "@ant-design/icons";
 import {
   AppstoreOutlined,
   MenuUnfoldOutlined,
@@ -8,25 +8,36 @@ import {
   DesktopOutlined,
   ContainerOutlined,
   MailOutlined,
-  MenuOutlined
-} from '@ant-design/icons';
-import React from 'react';
-
+  MenuOutlined,
+} from "@ant-design/icons";
+import React from "react";
 
 const menu = (
   <Menu>
     <Menu.Item>
-      <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.antgroup.com"
+      >
         1st menu item
       </a>
     </Menu.Item>
     <Menu.Item icon={<DownOutlined />} disabled>
-      <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.aliyun.com"
+      >
         2nd menu item (disabled)
       </a>
     </Menu.Item>
     <Menu.Item disabled>
-      <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.luohanacademy.com"
+      >
         3rd menu item (disabled)
       </a>
     </Menu.Item>
@@ -35,14 +46,17 @@ const menu = (
 );
 
 export const SideMenu = () => {
-  return(
+  return (
     <>
-    <Dropdown overlay={menu}>
-      <a style={{size: "large", color: "white"}} className="ant-dropdown-link" onClick={e => e.preventDefault()}>
-        <MenuOutlined />
-      </a>
-      
-    </Dropdown>
-  </>
+      <Dropdown overlay={menu}>
+        <a
+          style={{ size: "large", color: "white" }}
+          className="ant-dropdown-link"
+          onClick={(e) => e.preventDefault()}
+        >
+          <MenuOutlined />
+        </a>
+      </Dropdown>
+    </>
   );
 };
