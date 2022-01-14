1baefe903886f7065bb27b7ebbd7fc58d9c39183
zhuang-jia-xu <76544194+zhuang-jia-xu@users.noreply.github.com>
2022-01-14 12:12:08 +0800

    refactor prolist

--- frontend/src/components/Calendar/index.js
+++ frontend/src/components/Calendar/index.js
@@ -1,22 +1,19 @@
-import { useRef, useEffect } from "react";
 import ModAwesomeCalendar from "./AwesomeCalendar";
 import "react-calendar/dist/Calendar.css";
 import { Text } from "..";
 
 export const Calendar = () => {
-  const calendarRef = useRef();
-  
   return (
     <>
       <Text.SectionTitle.Black>Calendar</Text.SectionTitle.Black>
-      <div 
-          style={{
-            backgroundColor: "#fff",
-            padding: "1vh 1vw 1vh 1vw",
-            borderRadius: "2px",
+      <div
+        style={{
+          backgroundColor: "#fff",
+          padding: "1vh 1vw 1vh 1vw",
+          borderRadius: "2px",
         }}
       >
-        <ModAwesomeCalendar ref={calendarRef} />
+        <ModAwesomeCalendar />
       </div>
     </>
   );
--- frontend/src/components/PostList/index.js
+++ frontend/src/components/PostList/index.js
@@ -1,91 +1,76 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Tag } from "antd";
 import { MessageOutlined, LikeOutlined, StarOutlined } from "@ant-design/icons";
 import ProList from "@ant-design/pro-list";
 import { Text } from "..";
+import styles from "./styles.module.css";
+import faker from "faker";
 
 const IconText = ({ icon, text }) => (
-  <span>
+  <span style={{ marginRight: 8 }}>
     {React.createElement(icon, { style: { marginRight: 8 } })}
     {text}
   </span>
 );
 
-const dataSource = [
-  {
-    title: "语雀的天空",
-  },
-  {
-    title: "Ant Design",
-  },
-  {
-    title: "蚂蚁金服体验科技",
-  },
-  {
-    title: "TechUI",
-  },
-];
-
 export const PostList = () => {
+  const dataSource = [];
+
+  useEffect(() => {
+    for (let i = 0; i < 10; i++) {
+      dataSource.push({
+        title: faker.lorem.sentence(),
+        content: faker.lorem.paragraph(),
+        description: (
+          <>
+            <Tag>语雀专栏</Tag>
+            <Tag>设计语言</Tag>
+            <Tag>蚂蚁金服</Tag>
+          </>
+        ),
+        extra: <img width={272} alt="logo" src={faker.image.image()} />,
+        actions: [
+          <div className={styles.iconWrapper}>
+            {[
+              <IconText
+                icon={StarOutlined}
+                text="156"
+                key="list-vertical-star-o"
+                className={styles.icon}
+              />,
+              <IconText
+                icon={LikeOutlined}
+                text="156"
+                key="list-vertical-like-o"
+                className={styles.icon}
+              />,
+              <IconText
+                icon={MessageOutlined}
+                text="2"
+                key="list-vertical-message"
+                className={styles.icon}
+              />,
+            ]}
+          </div>,
+        ],
+      });
+    }
+  });
+
   return (
     <>
       <Text.SectionTitle.Black>Posts</Text.SectionTitle.Black>
       <ProList
-        toolBarRender={() => {}}
         itemLayout="vertical"
         rowKey="id"
         headerTitle=""
         dataSource={dataSource}
         metas={{
           title: {},
-          description: {
-            render: () => (
-              <>
-                <Tag>语雀专栏</Tag>
-                <Tag>设计语言</Tag>
-                <Tag>蚂蚁金服</Tag>
-              </>
-            ),
-          },
-          actions: {
-            render: () => [
-              <IconText
-                icon={StarOutlined}
-                text="156"
-                key="list-vertical-star-o"
-              />,
-              <IconText
-                icon={LikeOutlined}
-                text="156"
-                key="list-vertical-like-o"
-              />,
-              <IconText
-                icon={MessageOutlined}
-                text="2"
-                key="list-vertical-message"
-              />,
-            ],
-          },
-          extra: {
-            render: () => (
-              <img
-                width={272}
-                alt="logo"
-                src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-              />
-            ),
-          },
-          content: {
-            render: () => {
-              return (
-                <div>
-                  段落示意：蚂蚁金服设计平台
-                  design.alipay.com，用最小的工作量，无缝接入蚂蚁金服生态，提供跨越设计与开发的体验解决方案。蚂蚁金服设计平台
-                  design.alipay.com，用最小的工作量，无缝接入蚂蚁金服生态提供跨越设计与开发的体验解决方案。
-                </div>
-              );
-            },
-          },
+          description: {},
+          actions: {},
+          extra: {},
+          content: {},
         }}
       />
     </>
