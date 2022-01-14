22c191fc3c2b2d591e1b00645edbf043ecfe6317
BingCheng <b09901044@ntu.edu.tw>
2022-01-14 22:18:19 +0800

    Calendar comments

--- frontend/src/components/Calendar/index.js
+++ frontend/src/components/Calendar/index.js
@@ -2,30 +2,17 @@ import ModAwesomeCalendar from "./AwesomeCalendar";
 import "react-calendar/dist/Calendar.css";
 import { Text } from "..";
 
-const exampleEvents = [
-  {
-    id: 1,
-    color: "#fd3153",
-    from: "2022-01-02T18:00:00+00:00",
-    to: "2022-01-03T19:00:00+00:00",
-    title: "部課",
-  },
-  {
-    id: 2,
-    color: "#1ccb9e",
-    from: "2022-01-11T13:00:00+00:00",
-    to: "2022-01-15T14:00:00+00:00",
-    title: "MakeNTU",
-  },
-  {
+/**
+ * 
+ * @param {*} [{
     id: 3,
     color: "#3694DF",
     from: "2022-01-25T13:00:00+00:00",
     to: "2022-01-25T20:00:00+00:00",
     title: "專題說明會",
-  },
-];
-
+  },] 
+ * @returns 
+ */
 export const Calendar = ({events}) => {
   return (
     <>
