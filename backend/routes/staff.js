aa7d4f9ef3b73845a97a105392e9abfe1abc0cc9
Davis Chou <lxdfs58@gmail.com>
2022-01-15 19:59:45 +0800

    fix serach page margin

--- backend/routes/staff.js
+++ backend/routes/staff.js
@@ -84,68 +84,6 @@ router.post("/signup", (req, res) => {
 router.get("/search", (req, res) => {
   const { search, type, location } = req.query;
   itemQuery(search, type, location, res);
-  //
-  // const { search, type, location } = req.query;
-  // console.log(
-  //   `search index : ${search} / type : ${type} / location : ${location} `
-  // );
-  // let searchResult = [],
-  //   typeResult = [],
-  //   Result = [];
-  // const sheetdata = [
-  //   ["STM32", "board", "A", "10"],
-  //   ["MPU6050", "module", "B", "15"],
-  //   ["MG-90", "motor", "C", "20"],
-  // ];
-  // if (search) {
-  //   sheetdata.forEach((value, i) => {
-  //     if (value[0].includes(search)) {
-  //       searchResult.push(value);
-  //     }
-  //   });
-  //   if (!searchResult) {
-  //   }
-  // } else {
-  //   searchResult = sheetdata;
-  // }
-  // if (type && type != "all") {
-  //   searchResult.forEach((value, i) => {
-  //     if (value[1].includes(type)) {
-  //       typeResult.push(value);
-  //     }
-  //   });
-  //   if (!typeResult) {
-  //   }
-  // } else {
-  //   typeResult = searchResult;
-  // }
-  // if (location && location != "all") {
-  //   typeResult.forEach((value, i) => {
-  //     if (value[2].includes(location)) {
-  //       Result.push({
-  //         key: `E${i}`,
-  //         name: value[0],
-  //         type: value[1],
-  //         location: value[2],
-  //         quantity: value[3],
-  //       });
-  //     }
-  //   });
-  //   if (!Result) {
-  //   }
-  // } else {
-  //   typeResult.forEach((value, i) => {
-  //     Result.push({
-  //       key: `E${i}`,
-  //       name: value[0],
-  //       type: value[1],
-  //       location: value[2],
-  //       quantity: value[3],
-  //     });
-  //   });
-  // }
-  // console.log({ data: Result });
-  // res.send({ data: Result });
 });
 
 export default router;
