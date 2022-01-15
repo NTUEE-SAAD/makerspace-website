9ef1b6524621218873d209f9d957f6d340a82cd0
506f6f68 <louis020608@gmail.com>
2022-01-16 01:11:35 +0800

    staffheader

--- frontend/src/components/StaffHeader.js
+++ frontend/src/components/StaffHeader.js
@@ -3,20 +3,29 @@ import { UserOutlined } from "@ant-design/icons";
 import { LogoLink } from ".";
 import { Link } from "react-router-dom";
 import instance from "../instance";
+import styled from "styled-components";
+
+const LogoImage = styled.img`
+  height: 54px;
+  width: auto;
+  transform: translate(-20px, -2px);
+`;
 
 export const StaffHeader = () => {
   return (
     <Row justify="space-around" align="center">
       <Col span={6}>
-        <LogoLink />
+        <Link to="/staff">
+          <LogoImage src="/mkslogo-h.png" />
+        </Link>
       </Col>
       <Col span={18}>
         <Menu mode="horizontal" theme="dark" style={{ float: "right" }}>
           <Menu.Item key="1">
-            <Link to="/3dp">3DP</Link>
+            <Link to="/staff/3dp">3DP</Link>
           </Menu.Item>
           <Menu.Item key="2">
-            <Link to="/laser">Laser</Link>
+            <Link to="/staff/laser">Laser</Link>
           </Menu.Item>
           <Menu.Item key="3">
             <Link to="/staff/items">Item</Link>
